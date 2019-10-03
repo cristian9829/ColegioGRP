e7e7f9cef9be9d49ab42740395120b549a550de5
Cristian Clavijo <cristian982940@gmail.com>
2019-10-02 22:07:47 -0500

    Dev: add section about and show map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,92 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+function map(){
+	//======================= Code JavaScript, API MapBoxGl ======================================
+
+	mapboxgl.accessToken = 'pk.eyJ1IjoiY3Jpc3RpYW4tY2wiLCJhIjoiY2pzYWt3b3FyMDBqcTQzbzdlZ2xjM3FrayJ9.n216NBqfSJBaWNeUnNIUaA';
+		var map = new mapboxgl.Map({
+		container: 'map',
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [ -74.0920055,  4.5890004], // starting position
+		zoom: 15.3,
+		pitch: 45,
+		bearing: -17.6
+	});
+
+	function rotateCamera(timestamp) {
+		// clamp the rotation between 0 -360 degrees
+		// Divide timestamp by 100 to slow rotation to ~10 degrees / sec
+		map.rotateTo((timestamp / 100) % 360, {duration: 0});
+		// Request the next frame of the animation.
+		requestAnimationFrame(rotateCamera);
+	}
+
+	map.on('load', function() {
+		// Insert the layer beneath any symbol layer.
+		var layers = map.getStyle().layers;
+		// disable map zoom when using scroll
+		 
+		var labelLayerId;
+		for (var i = 0; i < layers.length; i++) {
+			if (layers[i].type === 'symbol' && layers[i].layout['text-field']) {
+			labelLayerId = layers[i].id;
+			break;
+			}
+		}
+			 
+		map.addLayer({
+			'id': '3d-buildings',
+			'source': 'composite',
+			'source-layer': 'building',
+			'filter': ['==', 'extrude', 'true'],
+			'type': 'fill-extrusion',
+			'minzoom': 15,
+			'paint': {
+				'fill-extrusion-color': '#aaa',
+				 
+				// use an 'interpolate' expression to add a smooth transition effect to the
+				// buildings as the user zooms in
+				'fill-extrusion-height': [
+					"interpolate", ["linear"], ["zoom"],
+					15, 0,
+					15.05, ["get", "height"]
+				],
+				'fill-extrusion-base': [
+					"interpolate", ["linear"], ["zoom"],
+					15, 0,
+					15.05, ["get", "min_height"]
+				],
+				'fill-extrusion-opacity': .6
+			}
+		}, labelLayerId);
+	});
+
+	map.loadImage('https://cdn2.iconfinder.com/data/icons/pittogrammi/142/94-512.png', function(error, image) {
+		if (error) throw error;
+			map.addImage('location', image);
+			map.addLayer({
+				"id": "points",
+				"type": "symbol",
+				"source": {
+					"type": "geojson",
+					"data": {
+						"type": "FeatureCollection",
+						"features": [{
+						"type": "Feature",
+							"geometry": {
+							"type": "Point",
+							"coordinates": [ -74.0920055,  4.5890004]
+							}
+						}]
+					}
+				},
+				"layout": {
+				"icon-image": "location",
+				"icon-size": 0.10
+				}
+			});
+		});
+	//======================= Code JavaScript, API MapBoxGl ======================================
+}
